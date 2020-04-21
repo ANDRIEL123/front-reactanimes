fecc6628e615082d27203b8d1f8a362f964721c5
andriel123 <andrielmaicom@hotmail.com>
2020-04-21 19:35:48 -0300

    Criado parte do DashBoard com filtro de animes

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -12,31 +12,73 @@ const viewLocalStorage = () => {
 }
 */
 
-
-
-
 export default class Menu extends Component {
     state = {
         animes: [],
+        animesAux: [],
         search: ''
     }
 
     loadAnimesApi = async () => {
-        const response = await api
+        const response = await api('/animes')
+        this.setState({ animes: response.data.response, animesAux: response.data.response })
+        console.log(response.data.response)
+    }
+
+    componentDidMount() {
+        this.loadAnimesApi()
     }
 
     handleChangeSearch = (e) => {
         this.setState({ search: e.target.value })
-        console.log(e.target.value)
     }
 
+    filterAnimes = (animes, query) => {
+        var filteredArray = animes.filter(anime => {
+            return anime.title.toLowerCase().indexOf(query.toLowerCase()) >= 0;
+        });
+        this.setState({ animesAux: filteredArray })
+    };
+
+
     render() {
+        const { animes, animesAux, search } = this.state
         return (
             <div className="main-menu">
                 < Header /><br></br>
+                <div>
+                    <Button variant="contained" type="submit" color="primary" onClick={""} >Incluir um novo anime</Button>
+                </div>
                 <div className="search">
-                    <TextField label="Pesquise um anime" type="text" onChange={this.handleChangeSearch.bind(this)} />
-                    <Button variant="contained" type="submit" color="primary" onClick={"#"} >Incluir anime</Button>
+                    <TextField label="Pesquise um anime" onKeyDown={(event) => { if (event.keyCode === 13) { this.filterAnimes(animes, search) } }} type="text" onChange={this.handleChangeSearch.bind(this)} />
+                    <Button variant="contained" type="submit" color="primary" onClick={() => this.filterAnimes(animes, search)} >Pesquisar anime</Button>
+                </div>
+
+
+                <div className="animes">
+                    <h2>Listagem dos Animes</h2>
+
+                    {animesAux.map(anime => (
+                        <div className="list-animes" key={anime._key}>
+                            <h3>{anime.title}</h3>
+                            <p>{anime.description}</p>
+                            <div className="buttons">
+                                <Button variant="contained" className="btn-edit" type="submit" color="primary" onClick={"#"} >Editar</Button>
+                                <Button variant="contained" className="btn-rem" type="submit" color="primary" onClick={"#"} >Remover</Button>
+                            </div>
+
+
+                        </div>
+
+                    ))}
+                    <div className="pages-animes">
+                        <Button variant="contained" className="btn-ant" type="submit" color="primary" onClick={"#"} >Página anterior</Button>
+                        <Button variant="contained" className="btn-prox" type="submit" color="primary" onClick={"#"} >Próxima Página</Button>
+                    </div>
+                    <div>
+                        <h3> by Andriel Friedrich © </h3>
+                    </div>
+
                 </div>
 
 
